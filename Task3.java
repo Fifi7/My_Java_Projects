6b71538cb21f16086570a8865bbf041cb833a7a0
Refilwe_Gololo <94147480+Fifi7@users.noreply.github.com>
2023-05-07 10:54:58 +0200

    A program that checks even and odd numbers.

---
+++ Task3.java
@@ -0,0 +1,20 @@
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println(task3(4));
+    }
+    public static String task3(int x) {
+        if (x % 2 == 0) {
+            return "even";
+        } else {
+                  return "odd";
+
+        }
+    }
+}
+
+
+
+
+
+
+
