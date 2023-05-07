b442e5f004a0e32e6993261e6d55ff3129d7795c
Refilwe_Gololo <94147480+Fifi7@users.noreply.github.com>
2023-05-07 10:59:30 +0200

    program converts the Celsius Temperature to Fahrenheit Temperature.

---
+++ Task7.java
@@ -0,0 +1,13 @@
+// This program converts the Celsius Temperature to Fahrenheit Temperature:
+public class Task7 {
+    public static void main (String[] args){
+        System.out.println(task7(40));
+
+    }
+      static int task7(int celsius){
+         return ((9*celsius)/5)+32;    // This is the formula that converts the Celsius to Fahrenheit.
+                                       // task7 method will return the converted Celsius temperature
+                                       // to Fahrenheit temperature.
+
+    }
+}
